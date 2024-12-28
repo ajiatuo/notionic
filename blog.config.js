b1629063ea509357e0563835e77423355aa49a0a
ajiatuo.cn <wushufengshuo@gmail.com>
2024-12-29 06:51:35 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,8 +1,8 @@
 const BLOG = {
   title: 'Notionic',
-  author: '左蓝',
-  email: 'i@zuolan.me',
-  link: 'https://zuolan.me',
+  author: '伽陀',
+  email: 'ajiatuo@awaker.edu.kg',
+  link: 'https://ajiatuo.cn',
   newsletter: 'Notionic Weekly',
   description: 'A static blog build on top of Notion and Next.js',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']

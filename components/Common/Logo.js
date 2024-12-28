f50e41e8cd8f893982d281e08e5b004e0e00b4e7
ajiatuo.cn <wushufengshuo@gmail.com>
2024-12-29 07:21:31 +0800

    Update Logo.js

--- components/Common/Logo.js
+++ components/Common/Logo.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 
 const Logo = (props) => (
   <svg
-    xmlns='http://www.w3.org/2000/svg'
+    xmlns='https://image.ajiatuo.cn/favicon.svg'
     width='24'
     height='24'
     viewBox='0 0 100 100'
